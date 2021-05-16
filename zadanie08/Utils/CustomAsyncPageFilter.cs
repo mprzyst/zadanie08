930c6aec58a45ca86068adfa3b7de5f498de7a0d
mprzyst <59484241+mprzyst@users.noreply.github.com>
2021-05-16 20:39:10 +0200

    Add project files.

---
+++ zadanie08/Utils/CustomAsyncPageFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zadanie08.Utils
+{
+    public class CustomAsyncPageFilter : IAsyncPageFilter
+    {
+        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+        {
+
+            await next.Invoke();
+        }
+
+        public async Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ zadanie08/Utils/CustomFilterAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zadanie08.Utils
+{
+    public class CustomFilterAttribute : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            
+           var ipAddress = context.HttpContext.Connection.RemoteIpAddress;
+
+            ipAddress = System.Net.Dns.GetHostEntry(ipAddress).AddressList.First(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+            ipAddress.ToString();
+
+            var result = context.Result;
+
+            if(result is PageResult)
+            {
+                var page = ((PageResult)result);
+                page.ViewData["filterMessage"] = "Your IP address is: " + ipAddress;
+            }
+
+            await next.Invoke();
+
+        }
+    }
+}
